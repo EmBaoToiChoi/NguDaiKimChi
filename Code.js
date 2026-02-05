cf3766db232c175e46f56b293d70d0d8e937c258
Hoai Bao <hoaibao200611423@gmail.com>
2026-02-05 22:10:13 +0700

    Fix video file paths and maintain slider functionality

--- Code.js
+++ Code.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   const videos = [
-    "./1.mp4",
-    "./2.mp4",
-    "./3.mp4",
-    "./4.mp4",
-    "./5.mp4",
-    "./6.mp4",
-    "./7.mp4",
+    "./1..mp4",
+    "./2..mp4",
+    "./3..mp4",
+    "./4..mp4",
+    "./5..mp4",
+    "./6..mp4",
+    "./7..mp4",
   ];
 
   let index = 0;
@@ -89,6 +89,7 @@ document.addEventListener("DOMContentLoaded", function () {
     offsetBody();
     window.addEventListener("resize", offsetBody);
 });
+
 
 
 
