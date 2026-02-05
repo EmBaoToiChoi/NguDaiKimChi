110a7aff9055131728a587f13aa99fa3eca66ff7
Hoai Bao <hoaibao200611423@gmail.com>
2026-02-05 22:02:28 +0700

    Update Code.js

--- Code.js
+++ Code.js
@@ -1,11 +1,11 @@
 const videos = [
-  "1..mp4",
-  "2..mp4",
-  "3..mp4",
-  "4..mp4",
-  "5..mp4",
-  "6..mp4",
-  "7..mp4",
+  "./1..mp4",
+  "./2..mp4",
+  "./3..mp4",
+  "./4..mp4",
+  "./5..mp4",
+  "./6..mp4",
+  "./7..mp4",
 ];
 
 let index = 0;
@@ -82,5 +82,6 @@ document.addEventListener("DOMContentLoaded", function () {
     offsetBody();
     window.addEventListener("resize", offsetBody);
 });
+
 
 
